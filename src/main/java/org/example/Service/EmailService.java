f6509703ec8e2c5e175ebf788ba08c94d76d74e1
Elizaweta <liza_pozdn03@mail.ru>
2025-02-10 13:34:54 +0700

    Добавление сервиса почты

---
+++ src/main/java/org/example/Service/EmailService.java
@@ -0,0 +1,22 @@
+package org.example.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+/**
+ * Сервис для отправки электронных писем
+ */
+@Service
+public class EmailService {
+    @Autowired
+    public JavaMailSender emailSender;
+    public void sendSimpleEmail(String toAddress, String subject, String message) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(toAddress);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(message);
+        emailSender.send(simpleMailMessage);
+    }
+}
