b1293d9390a05f8fb5a6eee9912c629624a2313a
Elizaweta <liza_pozdn03@mail.ru>
2025-02-10 14:51:01 +0700

    Реализация генерации пароля

--- src/main/java/org/example/Service/EmailService.java
+++ src/main/java/org/example/Service/EmailService.java
@@ -1,22 +1,85 @@
 package org.example.Service;
 
+import org.passay.CharacterData;
+import org.passay.CharacterRule;
+import org.passay.EnglishCharacterData;
+import org.passay.PasswordGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 /**
  * Сервис для отправки электронных писем
  */
 @Service
 public class EmailService {
+    /**
+     * Заголовок сообщения
+     */
+    private final String subject = "Пароль для доступа к режиму администратора";
+    /**
+     * Текст сообщения
+     */
+    private final String message = "Для входа в режим администратора введите пароль: ";
+    /**
+     * Длина пароля
+     */
+    private final int length = 20;
+    /**
+     * Количество заглавных букв в пароле
+     */
+    private final int countUpperCase = 2;
+    /**
+     * Количество цифр в пароле
+     */
+    private final int countDigit = 3;
+    /**
+     * Количество спец символов в пароле
+     */
+    private final int countSpecial = 1;
     @Autowired
     public JavaMailSender emailSender;
-    public void sendSimpleEmail(String toAddress, String subject, String message) {
+
+    /**
+     * Метод отправки электронного письма
+     * @param toAddress адрес электронной почты
+     * @return отправленный пароль
+     */
+    public String sendEmail(String toAddress) {
+        String password = generatePassword();
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setTo(toAddress);
         simpleMailMessage.setSubject(subject);
-        simpleMailMessage.setText(message);
+        simpleMailMessage.setText(message+password);
         emailSender.send(simpleMailMessage);
+        return password;
+    }
+    /**
+     * Генерация пароля пароля
+     * @return сгенерированный пароль
+     */
+    private String generatePassword(){
+        PasswordGenerator passwordGenerator = new PasswordGenerator();  //генератор паролей
+
+        ArrayList<CharacterRule> rules = new ArrayList<>();
+        rules.add(new CharacterRule(EnglishCharacterData.UpperCase, countUpperCase)); //2 заглавные буквы
+        rules.add(new CharacterRule(EnglishCharacterData.LowerCase, length-countDigit-countSpecial-countUpperCase));    //14 строчных букв
+        rules.add(new CharacterRule(EnglishCharacterData.Digit, countDigit));    //3 цифры
+        CharacterRule specialCharacterRule = new CharacterRule(new CharacterData() {    //1 символ из списка
+            @Override
+            public String getErrorCode() {
+                return "SAMPLE_ERROR_CODE";
+            }
+
+            @Override
+            public String getCharacters() {
+                return "-!?*+=/";
+            }
+        }, countSpecial);
+        rules.add(specialCharacterRule);
+        return passwordGenerator.generatePassword(length, rules);
     }
 }
